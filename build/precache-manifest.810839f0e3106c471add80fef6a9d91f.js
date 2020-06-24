7614f31a3178e89170c30609369741b2b217c567
Reskyppo <reskybeen@gmail.com>
2020-06-24 22:36:50 +0700

    initial commit

---
+++ build/precache-manifest.810839f0e3106c471add80fef6a9d91f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dd4853588b9fe24416019d7d1d1fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693ae7bc7842a588ecbc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e8b6c1c0b8fb21dd3ba",
+    "url": "/static/css/main.0a5b2c2c.chunk.css"
+  },
+  {
+    "revision": "693ae7bc7842a588ecbc",
+    "url": "/static/js/2.9b33aeca.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/static/js/2.9b33aeca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8b6c1c0b8fb21dd3ba",
+    "url": "/static/js/main.4ab374ee.chunk.js"
+  },
+  {
+    "revision": "edf0f2bc8f58366e731f",
+    "url": "/static/js/runtime-main.af490a56.js"
+  }
+]);
